fbe61e4991a7a0b33ccdd255dbaaeff8305fa1f8
Vanessa Borges <89138836+nessaborges@users.noreply.github.com>
2022-01-21 11:30:03 -0300

    Add files via upload

---
+++ sucessor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class exer3
+{
+    static void Main()
+    {
+        int numdigitado;
+        int result;
+
+        Console.Write("Digite um numero:");
+        numdigitado = int.Parse(Console.ReadLine());
+
+        result = numdigitado+1;
+
+        Console.WriteLine("O seu sucessor é:" + result);
+    }
+}
