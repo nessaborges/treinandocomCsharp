2d0172fe82f3a4063344e8b59b93421609217e40
Vanessa Borges <89138836+nessaborges@users.noreply.github.com>
2022-01-21 11:09:20 -0300

    Add files via upload

---
+++ somando-dois-nums.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class exer2
+{
+    static void Main()
+    {
+        int n1, n2, result;
+
+        Console.Write("Digite um numero:");
+        n1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite outro numero:");
+        n2 = int.Parse(Console.ReadLine());
+
+        result = (n1 + n2);
+
+        Console.WriteLine("O Resultado da soma é:" + result);
+
+    }
+}
