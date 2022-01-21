80a4bcb52f9166ce9929f386fa7554002aaf803f
Vanessa Borges <89138836+nessaborges@users.noreply.github.com>
2022-01-21 11:50:04 -0300

    Add files via upload

---
+++ media-nota.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class exer4
+{
+    static void Main()
+    {
+        int nota1, nota2;
+        float media;
+
+        Console.Write("digite sua primeira nota:");
+        nota1 = int.Parse(Console.ReadLine());
+
+        Console.Write("digite sua segunda nota:");
+        nota2 = int.Parse(Console.ReadLine());
+
+        media = ((nota1+nota2)/2);
+
+        Console.WriteLine("Sua média é:" + media);
+
+
+
+    }
+}
