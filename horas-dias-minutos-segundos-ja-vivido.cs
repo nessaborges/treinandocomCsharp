13982d8b287de163ae37f36e75991c2c96e4fa9c
Vanessa Borges <89138836+nessaborges@users.noreply.github.com>
2022-01-21 10:20:54 -0300

    Add files via upload

---
+++ horas-dias-minutos-segundos-ja-vivido.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class exer
+{
+    static void Main()
+    {
+        int idade;
+        int dias;
+        int horas;
+        int minutos;
+        int segundos;
+
+        Console.Write("Digite sua idade");
+        idade = int.Parse(Console.ReadLine());
+        dias = idade * 365;
+        horas = dias * 24;
+        minutos = horas * 60;
+        segundos = minutos * 60;
+
+        Console.WriteLine("dias:" + dias);
+        Console.WriteLine("horas:" + horas);
+        Console.WriteLine("minutos:"+ minutos);
+        Console.WriteLine("segundos:" + segundos);
+
+    }
+}
